6eb1de76798233f206a6d322c89083a156fcc8da
PeshkovDmitry <peshkov-dv@yandex.ru>
2023-04-19 07:05:32 +0300

    Готовая программа, строки вводятся и выводятся через запятую

--- Program.cs
+++ Program.cs
@@ -3,13 +3,14 @@
 //  либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться 
 //  коллекциями, лучше обойтись исключительно массивами.
 
-Console.Write("Введите массив строк: ");
-String[] inputArray = Console.ReadLine().Split(" ");
+Console.Write("Введите через запятую массив строк: ");
+String[] inputArray = Console.ReadLine().Split(",");
 String[] outputArray = new String[inputArray.Length];
 int outputArraySize = 0;
 foreach (String s in inputArray) 
 {
     if (s.Length <=3) outputArray[outputArraySize++] = s;
 }
-if (outputArraySize > 0) Console.WriteLine(String.Join(" ",outputArray));
+outputArray = outputArray.SkipLast(outputArray.Length - outputArraySize).ToArray();
+if (outputArraySize > 0) Console.WriteLine(String.Join(", ",outputArray));
 else Console.WriteLine("Все элементы исходного массива длиннее 3-х символов");
