43df2b7ce794070eee14e47e2b4d87a7b52bf02d
PeshkovDmitry <peshkov-dv@yandex.ru>
2023-04-19 06:50:58 +0300

    Готовая программа, строки вводятся через пробел, т.е. сами пробел содержать не могут

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//  Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+//  длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+//  либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться 
+//  коллекциями, лучше обойтись исключительно массивами.
+
+Console.Write("Введите массив строк: ");
+String[] inputArray = Console.ReadLine().Split(" ");
+String[] outputArray = new String[inputArray.Length];
+int outputArraySize = 0;
+foreach (String s in inputArray) 
+{
+    if (s.Length <=3) outputArray[outputArraySize++] = s;
+}
+if (outputArraySize > 0) Console.WriteLine(String.Join(" ",outputArray));
+else Console.WriteLine("Все элементы исходного массива длиннее 3-х символов");
